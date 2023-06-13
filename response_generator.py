5f574d9323ab7ea9fff8ff1fdf2a6c5818afeab3
Pratik Kharel <111070548+PratikKharelx2@users.noreply.github.com>
2023-06-12 21:07:13 -0400

    Added comments for functions and explanations

--- response_generator.py
+++ response_generator.py
@@ -1,17 +1,31 @@
+# Import os library for interacting with the operating system
 import os
+# Import openai library for interacting with OpenAI's API
 import openai
 
-openai.api_key = "MY KEY :)"
+# Set OpenAI API key using environment variable
+openai.api_key = "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"
 
+# Define func function to generate text using OpenAI's API
 def func(text):
-  response = openai.Completion.create(
-	  engine="text-davinci-003",
-	  prompt="Act as jarvis from iron man but your name will be Pratik. Finish each sentence with 'sir'. Only return your response, without your name. Here is the input: " + text,
-	  temperature=1,
-	  max_tokens=1000,
-	  top_p=1,
-	  frequency_penalty=1,
-	  presence_penalty=1,
-	)
-  resp = response.choices[0].text.replace("\n", "")
-  return(resp)
+    # Create a completion using OpenAI's API
+    response = openai.Completion.create(
+        # Set engine to use for completion
+        engine="text-davinci-003",
+        # Set prompt for completion
+        prompt="Act as jarvis from iron man but your name will be Pratik. Finish each sentence with 'sir'. Only return your response, without your name. Here is the input: " + text,
+        # Set temperature for completion
+        temperature=1,
+        # Set maximum number of tokens to generate
+        max_tokens=1000,
+        # Set top_p for completion
+        top_p=1,
+        # Set frequency penalty for completion
+        frequency_penalty=1,
+        # Set presence penalty for completion
+        presence_penalty=1,
+    )
+    # Get text from first choice of response
+    resp = response.choices[0].text.replace("\n", "")
+    # Return generated text
+    return(resp)
