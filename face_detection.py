f1d0e955bcf0b1d7e758339ca22b09f100a04f87
Pratik Kharel <111070548+PratikKharelx2@users.noreply.github.com>
2023-06-12 21:09:01 -0400

    Added comments for functions and explanations

--- face_detection.py
+++ face_detection.py
@@ -1,13 +1,24 @@
+# Import OpenCV library for computer vision tasks
 import cv2
+# Import os library for interacting with the operating system
 import os
 
+# Create face cascade classifier using pre-trained Haar cascade classifier
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
+# Define face_detection function to detect faces in a given frame
 def face_detection(frame):
+    # Convert frame from BGR to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Detect faces in grayscale frame using face cascade classifier
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    # Loop through each detected face
     for i, (x,y,w,h) in enumerate(faces):
+        # Draw rectangle around face on frame
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        # Extract face from frame
         face = frame[y:y+h, x:x+w]
+        # Save face to file
         cv2.imwrite(os.path.join("faces", f"face_{i}.jpg"), face)
+    # Return modified frame with rectangles drawn around faces
     return frame
